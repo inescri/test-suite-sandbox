401c638db65a24fd3b74137da9809de031c65bc8
Crisdell James Ines <crisdelli@codev.com>
2025-10-31 11:24:51 +0800

    increase timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   chromeWebSecurity: false,
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 20_000,
   e2e: {
     specPattern: "e2e-tests/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
